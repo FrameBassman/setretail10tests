ccd820af55e23cd4493420b8d932b7a644342a42
Dmitry Romashov <framebassman@gmail.com>
2021-08-11 12:41:20 +0300

    sample

--- SetRetail10_SCO/SetRobot/setrobot-ncr/src/main/java/ru/crystals/sco/setrobot/ncr/Sample.java
+++ SetRetail10_SCO/SetRobot/setrobot-ncr/src/main/java/ru/crystals/sco/setrobot/ncr/Sample.java
@@ -4,12 +4,20 @@ import com.codeborne.selenide.SelenideConfig;
 import com.codeborne.selenide.SelenideDriver;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class Sample {
-    @Test
+//    @Test
     public void e2eTest() throws Exception {
         SelenideConfig selenideConfig = new SelenideConfig();
         selenideConfig.browser(DriverProvider.class.getName());
         SelenideDriver selenide = new SelenideDriver(selenideConfig);
         selenide.open();
+    }
+
+    @Test
+    public void itWorks() throws Exception {
+        assertThat(1, equalTo(1));
     }
 }
