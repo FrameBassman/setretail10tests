ac0c8dd762cffe8aba4df152766de89193012430
Dmitry Romashov <framebassman@gmail.com>
2021-08-11 15:43:34 +0300

    Fix

--- SetRetail10_SCO/SetRobot/setrobot-ncr/src/main/java/ru/crystals/sco/setrobot/ncr/Sample.java
+++ SetRetail10_SCO/SetRobot/setrobot-ncr/src/main/java/ru/crystals/sco/setrobot/ncr/Sample.java
@@ -7,8 +7,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Sample {
     private SelenideDriver selenide;
+    protected Logger logger = LoggerFactory.getLogger(Sample.class);
 
     @Before
     public void setup() throws Exception {
@@ -23,5 +31,16 @@ public class Sample {
     public void e2eTest() throws Exception {
         SelenideElement main = selenide.find(By.name("SCOTInputWindow"));
         main.find(By.name("Shut Down SCOTAPP application")).click();
+    }
+
+    @Test
+    public void switchBetween() throws Exception {
+        Set<String> handles = selenide.getWebDriver().getWindowHandles();
+        for (Iterator<String> it = handles.iterator(); it.hasNext(); ) {
+            logger.info("Switch to the next window");
+            String handle = String.valueOf(it.next());
+            selenide.getWebDriver().switchTo().window(handle);
+            Thread.sleep(Duration.ofSeconds(2).toMillis());
+        }
     }
 }
