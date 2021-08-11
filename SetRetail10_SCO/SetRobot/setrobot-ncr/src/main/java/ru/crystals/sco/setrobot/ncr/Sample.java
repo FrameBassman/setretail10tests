d01a77a41d92c9b80e04c4b602bd959f43e10011
Dmitry Romashov <framebassman@gmail.com>
2021-08-11 12:26:08 +0300

    Add sample

---
+++ SetRetail10_SCO/SetRobot/setrobot-ncr/src/main/java/ru/crystals/sco/setrobot/ncr/Sample.java
@@ -0,0 +1,13 @@
+package ru.crystals.sco.setrobot.ncr;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class Sample {
+    @Test
+    public void itWorks() throws Exception {
+        assertThat(1, equalTo(2));
+    }
+}
