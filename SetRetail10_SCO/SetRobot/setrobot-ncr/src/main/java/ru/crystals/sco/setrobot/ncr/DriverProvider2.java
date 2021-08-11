59fd1c8e71e214950b0c1c2b925227fb551fe414
Dmitry Romashov <framebassman@gmail.com>
2021-08-11 15:21:03 +0300

    Demo

--- SetRetail10_SCO/SetRobot/setrobot-ncr/src/main/java/ru/crystals/sco/setrobot/ncr/DriverProvider2.java
+++ SetRetail10_SCO/SetRobot/setrobot-ncr/src/main/java/ru/crystals/sco/setrobot/ncr/DriverProvider2.java
@@ -9,14 +9,16 @@ import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class DriverProvider implements WebDriverProvider {
+public class DriverProvider2 implements WebDriverProvider {
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull DesiredCapabilities cap) {
+//        cap.setCapability("app","C:/windows/system32/calc.exe"); //если хотим сразу запускать какую-либо программу
         cap.setCapability("launchDelay","5"); //задержка после запуска программы
         cap.setCapability("debugConnectToRunningApp", true);
         try {
             return new RemoteWebDriver(new URL("http://172.29.17.8:9999"), cap);
+//            return new RemoteWebDriver(new URL("http://localhost:9999"), cap);
         } catch (MalformedURLException e) {
             throw new RuntimeException("Error in URL");
         }
